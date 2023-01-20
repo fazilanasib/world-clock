f55c8cc8b1babc913b6dfa5de08277d7453d5880
fazilanasib <121870855+fazilanasib@users.noreply.github.com>
2023-01-20 10:15:47 -0500

    fixed a small error in from Mh to h in updateCity function

--- java.js
+++ java.js
@@ -32,7 +32,7 @@ function updateCity(event) {
             <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
-            "Mh:mm:ss"
+            "h:mm:ss"
           )}<small>${cityTime.format("A")}</small></div>
         </div>`;
 }
